ba3b383d49f9e9f1cf583263f09eb56ca87626b2
suengwoo Lee <na1yeon33@gmail.com>
2021-06-07 07:19:51 +0900

    Physics Manager 와 Rigidbody

---
+++ UnityH/Assets/Scripts/GravityControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityControl : MonoBehaviour
+{
+    Rigidbody _rg;
+    public float _gravtyScale = 1;
+    float _gravityNomal = 9.81f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rg = GetComponent<Rigidbody>();
+        _rg.mass = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 자이로스코프의 값을 받아옴
+        // Vector3 vInput = Input.acceleration;
+        float gx = Input.GetAxis("Horizontal"); // -1 ~ 1;
+        float gy = Input.GetAxis("Vertical");
+        Vector3 g = new Vector3(gx, -1, gy);
+        g.Normalize();
+
+        Physics.gravity = g * _gravityNomal * _gravtyScale;
+        Debug.Log(Physics.gravity);
+    }
+}
