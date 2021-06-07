7c720c6d9c6b24317083d0a9a29498f4e877aa00
suengwoo Lee <na1yeon33@gmail.com>
2021-06-08 08:18:26 +0900

    Ray를 이용한 픽킹 방법

---
+++ UnityH/Assets/Scripts/RayTestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTestScript : MonoBehaviour
+{    
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            //Debug.Log("마우스 좌클릭 했어요");
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rHit;
+            int LMask = 1 << LayerMask.NameToLayer("Floor") | 1 << LayerMask.NameToLayer("Wall");
+            if(Physics.Raycast(r, out rHit, Mathf.Infinity, LMask))
+            {
+                Debug.Log(rHit.transform.name);
+                Debug.Log(rHit.point);
+            }
+        }
+    }
+}
