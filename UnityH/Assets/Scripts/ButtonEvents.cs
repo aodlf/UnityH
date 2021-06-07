6a9b408dcd0ac26e75d1199a2939f09cb6d944d2
suengwoo Lee <na1yeon33@gmail.com>
2021-06-08 07:04:27 +0900

    uGUI 많이 쓰는 기능2

---
+++ UnityH/Assets/Scripts/ButtonEvents.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEvents : MonoBehaviour
+{
+    public Text _dialog;
+    public Slider _slider;
+
+    public void ChangeDialog(string txt)
+    {
+        _dialog.text = txt;
+    }
+
+    void NowSliderValue()
+    {
+        Debug.Log("S_Value" + _slider.value);
+    }
+}
