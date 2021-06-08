ae9ac5cf5577d99abef266d48c908087f4ab167b
suengwoo Lee <na1yeon33@gmail.com>
2021-06-09 08:14:39 +0900

    Rigidbody를 이용한 발사체 구현

---
+++ UnityH/Assets/Scripts/CannonBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour
+{
+    public float _addForce = 1000;
+    Rigidbody _rgbd3D;
+
+    void Start()
+    {
+        _rgbd3D = GetComponent<Rigidbody>();
+        _rgbd3D.AddForce(transform.forward * _addForce);
+        Destroy(gameObject, 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityH/Assets/Scripts/LuncherControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuncherControl : MonoBehaviour
+{
+    public GameObject _objCannonBall;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            Instantiate(_objCannonBall, transform.position, transform.rotation);
+        }
+    }
+}
