90bf6314a35a31e37529e75b154226e25ebc2a91
suengwoo Lee <na1yeon33@gmail.com>
2021-06-09 07:23:10 +0900

    제어를 통한 GameObject의 이동과 회전 구현

---
+++ UnityH/Assets/Scripts/MoveControl2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveControl2 : MonoBehaviour
+{
+    public float _movSpeed = 10;
+    public float _rotSpeed = 150;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+        
+    {
+        float rx = Input.GetAxisRaw("Horizontal"); 
+        float mx = Input.GetAxisRaw("Horiz"); 
+        float mz = Input.GetAxisRaw("Vertical"); // -1 ~ 1
+        //float mx = Input.GetAxisRaw("Horizontal"); // -1 ~ 1
+
+        //Vector3 dir = (transform.forward * mz) + (transform.right * mx);
+        //dir = (dir.magnitude > 1) ? dir.normalized : dir;
+        //transform.position += dir * _movSpeed * Time.deltaTime;
+
+        //transform.position += transform.right * mx * _movSpeed * Time.deltaTime;
+        //transform.position += transform.forward * mz * _movSpeed * Time.deltaTime;
+
+        //Vector3 dir = new Vector3(mx, 0, mz);
+        //dir = (dir.magnitude > 1) ? dir.normalized : dir;
+        //transform.Translate(dir * _movSpeed * Time.deltaTime);
+
+        Vector3 dir = new Vector3(mx, 0, mz);
+        dir = (dir.magnitude > 1) ? dir.normalized : dir;
+        transform.Rotate(Vector3.up * rx * Time.deltaTime * _rotSpeed);
+        transform.Translate(dir * _movSpeed * Time.deltaTime);
+        //transform.Translate(Vector3.forward * mz * Time.deltaTime * _movSpeed);
+    }
+}
